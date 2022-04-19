86d6778a1cc8563c8b00537a4c177fdd0223493b
gebilxs <1799676669@qq.com>
2022-04-19 15:37:24 +0800

    add proof-of-work

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	bc := NewBlockchain()
+
 	bc.AddBlock("send 1 btc to vincent")
 	bc.AddBlock("sent more vincent")
 
 	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x \n", block.PrevBlockHash)
+		fmt.Printf("Prev	. hash: %x \n", block.PrevBlockHash)
 		fmt.Printf("Data: %s \n", block.Data)
 		fmt.Printf("Hash: %x \n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
